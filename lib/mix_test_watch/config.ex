28c6d7bd720ed372216f5d258fa6060c1722153c
Sascha Wolf <swolf.dev@gmail.com>
2017-09-15 14:05:48 +0200

    Config: Allow to pass configuration via the command line

--- lib/mix_test_watch/config.ex
+++ lib/mix_test_watch/config.ex
@@ -26,45 +26,86 @@ defmodule MixTestWatch.Config do
   Create a new config struct, taking values from the ENV
   """
   def new(cli_args \\ []) do
+    cli_config =
+      cli_args
+      |> OptionParser.parse!()
+      |> extract_config_from_cli_args()
+
+    cli_args = remove_watch_config(cli_args)
+
     %__MODULE__{
-      tasks:             get_tasks(),
-      clear:             get_clear(),
-      timestamp:         get_timestamp(),
-      runner:            get_runner(),
-      exclude:           get_excluded(),
-      cli_executable:    get_cli_executable(),
+      tasks:             get_tasks(cli_config),
+      clear:             get_clear(cli_config),
+      timestamp:         get_timestamp(cli_config),
+      runner:            get_runner(cli_config),
+      exclude:           get_excluded(cli_config),
+      cli_executable:    get_cli_executable(cli_config),
       cli_args:          cli_args,
-      extra_extensions:  get_extra_extensions(),
+      extra_extensions:  get_extra_extensions(cli_config),
     }
   end
 
 
-  defp get_runner do
+  defp extract_config_from_cli_args({configs, _}) do
+    configs
+    |> Keyword.get(:"watch.config")
+    |> extract_config_from_string()
+  end
+
+  defp extract_config_from_string(nil), do: %{}
+  defp extract_config_from_string(other) when not is_binary(other), do: %{}
+  defp extract_config_from_string(string) do
+    for config <- String.split(string, ";"), into: %{} do
+      case String.split(config, "=") do
+        [key, value] -> {key, Poison.decode!(value)}
+        [key] -> {key, true}
+      end
+    end
+  end
+
+  defp remove_watch_config(["--watch.config" | []]), do: []
+  defp remove_watch_config(["--watch.config" | t]) do
+    t
+    |> hd()
+    |> String.starts_with?("--")
+    |> if(do: t, else: tl(t))
+  end
+
+  defp remove_watch_config([h | t]), do: [h | remove_watch_config(t)]
+  defp remove_watch_config([]), do: []
+
+  defp get_runner(%{"runner" => cli}), do: cli
+  defp get_runner(_) do
     Application.get_env(:mix_test_watch, :runner, @default_runner)
   end
 
-  defp get_tasks do
+  defp get_tasks(%{"tasks" => cli}), do: cli
+  defp get_tasks(_) do
     Application.get_env(:mix_test_watch, :tasks, @default_tasks)
   end
 
-  defp get_clear do
+  defp get_clear(%{"clear" => cli}), do: cli
+  defp get_clear(_) do
     Application.get_env(:mix_test_watch, :clear, @default_clear)
   end
 
-  defp get_timestamp do
+  defp get_timestamp(%{"timestamp" => cli}), do: cli
+  defp get_timestamp(_) do
     Application.get_env(:mix_test_watch, :timestamp, @default_timestamp)
   end
 
-  defp get_excluded do
+  defp get_excluded(%{"excluded" => cli}), do: cli
+  defp get_excluded(_) do
     Application.get_env(:mix_test_watch, :exclude, @default_exclude)
   end
 
-  defp get_cli_executable do
+  defp get_cli_executable(%{"cli_executable" => cli}), do: cli
+  defp get_cli_executable(_) do
     Application.get_env(:mix_test_watch, :cli_executable,
                         @default_cli_executable)
   end
 
-  defp get_extra_extensions do
+  defp get_extra_extensions(%{}) do
     Application.get_env(:mix_test_watch, :extra_extensions,
                         @default_extra_extensions)
   end
